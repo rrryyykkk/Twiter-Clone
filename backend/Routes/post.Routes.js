e8e0744b0eb56cda62fc2a4f119956860da44710
rrryyykkk <rezkymubarok0256@gmail.com>
2024-12-24 14:22:07 +0700

    lanjutan backend

--- backend/Routes/post.Routes.js
+++ backend/Routes/post.Routes.js
@@ -1,12 +1,25 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectedRoute.js";
+import {
+  commentsPost,
+  createPost,
+  deletePost,
+  getAllPost,
+  getFollowing,
+  getLikesPost,
+  getUserPost,
+  likeUnlikePost,
+} from "../controllers/post.Controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/create", protectRoute,createPost)
-router.post("/like/:id", protectRoute,likeUnlikePost)
-router.post("/command", protectRoute,commandPost)
-router.delete("/", protectRoute,deletePost)
+router.get("/all",protectRoute, getAllPost)
+router.get("/following",protectRoute, getFollowing)
+router.get("/likes/:id",protectRoute, getLikesPost)
+router.get("/user/:username",protectRoute, getUserPost)
+router.post("/create", protectRoute, createPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comments/:id", protectRoute, commentsPost);
+router.delete("/:id", protectRoute, deletePost);
 
-
-export default router
+export default router;
