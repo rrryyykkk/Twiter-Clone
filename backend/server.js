5b64e226f11c2e235edeb5be2f6a482af92769d4
rrryyykkk <rezkymubarok0256@gmail.com>
2024-12-25 10:18:22 +0700

    final backend

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,12 @@ import dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
 import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.Routes.js";
-import usersRoute from "./routes/users.Routes.js";
-import postRoute from "./routes/post.Routes.js";
+// route
+import authRoutes from "./routes/auth.Route.js";
+import usersRoute from "./routes/users.Route.js";
+import postRoute from "./routes/post.Route.js";
+import notificationRoute from "./routes/notification.Route.js"
+
 import connectMongoDB from "./db/connectDb.js";
 
 const app = express();
@@ -28,6 +31,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoute);
 app.use("/api/post", postRoute);
+app.use("/api/notification", notificationRoute);
 
 app.listen(PORT, () => {
   console.log(`server is running in localhost:${PORT}`);
